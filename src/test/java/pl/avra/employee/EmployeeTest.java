da6bb31033e563d4d9753102f2f4ed74ac47365f
Mateusz Gepert <mateusz.gepert@avra.com.pl>
2017-03-27 14:38:51 +0200

    test in junit

---
+++ src/test/java/pl/avra/employee/EmployeeTest.java
@@ -0,0 +1,19 @@
+package pl.avra.employee;
+
+import org.junit.Test;
+
+public class EmployeeTest {
+
+
+    @Test
+    public void should_print() throws Exception {
+        // given
+        System.out.printf("test");
+
+        // when
+
+        // then
+    }
+
+
+}
